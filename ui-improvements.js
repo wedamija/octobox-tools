72f4571b8552232623641c5d83cf678da6d41626
Dan Fuller <dfuller@sentry.io>
2026-05-07 13:46:03 -0700

    update urls

--- ui-improvements.js
+++ ui-improvements.js
@@ -5,6 +5,8 @@
 // @description  Hide thread pane by default, show on notification select, drag to resize, close button to hide
 // @match        https://octobox.io/*
 // @grant        GM_addStyle
+// @updateURL    https://raw.githubusercontent.com/wedamija/octobox-tools/main/ui-improvements.js
+// @downloadURL  https://raw.githubusercontent.com/wedamija/octobox-tools/main/ui-improvements.js
 // ==/UserScript==
 
 (function () {
