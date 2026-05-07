7bf9074e574c3a6e5ba34528832045e101cbb03a
Dan Fuller <dfuller@sentry.io>
2026-05-07 13:48:48 -0700

    bump version

--- ui-improvements.js
+++ ui-improvements.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Octobox Resizable Thread Pane
 // @namespace    https://octobox.io
-// @version      2.1
+// @version      2.2
 // @description  Hide thread pane by default, show on notification select, drag to resize, close button to hide
 // @match        https://octobox.io/*
 // @grant        GM_addStyle
